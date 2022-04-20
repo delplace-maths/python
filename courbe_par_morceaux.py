18ce5a20c4ebb718754fad86383ab31d5fe7f36c
delplace-maths <60961270+delplace-maths@users.noreply.github.com>
2022-04-20 21:24:26 +0200

    Create courbe_par_morceaux.py

---
+++ courbe_par_morceaux.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.linspace(-2.5, 2, 100)
+y = x**2 - x -3
+plt.plot(x,y,c='b')
+
+x = np.linspace(2, 5, 100)
+y = x + 1
+plt.plot(x,y,c='b')
+
+x = 2
+y = -1
+plt.scatter(x,y,c='b')
+
+plt.xlim(-3, 3)
+plt.xticks(np.linspace(-3, 6,10))
+plt.yticks(np.linspace(-3, 8,12))
+plt.grid()
+plt.show()
